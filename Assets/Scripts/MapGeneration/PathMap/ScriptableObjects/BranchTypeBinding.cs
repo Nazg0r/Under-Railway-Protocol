f6383ae8b4ca9f46e81f778930aa087473542491
Nazarii <gorchinshiy83@gmail.com>
2025-11-04 15:42:53 +0200

    create ScriptableObjects for configure and setting path maps

---
+++ Assets/Scripts/MapGeneration/PathMap/ScriptableObjects/BranchTypeBinding.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+[Serializable]
+public struct BranchTypeBinding
+{
+	public int RailroadsCount;
+	public string TypeName;
+}
---
+++ Assets/Scripts/MapGeneration/PathMap/ScriptableObjects/BranchTypes.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Railroad/BranchTypes")]
+public class BranchTypes : ScriptableObject
+{
+	public List<BranchTypeBinding> Bindings;
+}
