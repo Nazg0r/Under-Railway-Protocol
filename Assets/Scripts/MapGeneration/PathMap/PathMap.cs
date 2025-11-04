927a88f7c083b4fc1cf4f924267cc2ba5287964f
Nazarii <gorchinshiy83@gmail.com>
2025-11-04 15:41:38 +0200

    implement PathMap and Station classes

---
+++ Assets/Scripts/MapGeneration/PathMap/PathMap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+public class PathMap : MapBase<Branch, Railroad>, IMapConnection<Station>
+{
+	public string Id { get; }
+	public Station FirstStation { get; }
+	public Station LastStation { get; }
+	public Branch InitialBranch { get; set; }
+
+	Station IMapConnection<Station>.FirstNode => FirstStation;
+	Station IMapConnection<Station>.LastNode => LastStation;
+
+	public PathMap(string id, Branch initialBranch, Station firstStation, Station lastStation)
+	{
+		Id = id;
+		InitialBranch = initialBranch;
+		FirstStation = firstStation;
+		LastStation = lastStation;
+	}
+
+	public List<Branch> GetBranches() =>
+		GetNodes(InitialBranch);
+
+	public List<Railroad> GetRailroads() =>
+		GetConnections(InitialBranch);
+
+
+	[CanBeNull]
+	public Branch GetBranchByName(string name) =>
+		GetNodeByName(InitialBranch, name);
+
+}
---
+++ Assets/Scripts/MapGeneration/Station.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class Station : IMapNode<PathMap>
+{
+	public string Name { get; }
+	public List<PathMap> Paths { get; } = new();
+
+	List<PathMap> IMapNode<PathMap>.Connections => Paths;
+
+	public Station(string name)
+	{
+		Name = name;
+	}
+}
