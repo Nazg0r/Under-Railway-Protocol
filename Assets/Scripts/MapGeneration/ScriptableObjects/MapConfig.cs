653d430473e3aebff28b9b0e7fcdad1ea88e1a12
Nazarii <gorchinshiy83@gmail.com>
2025-11-06 16:23:48 +0200

    create ScriptableObjects for Map configuration

---
+++ Assets/Scripts/MapGeneration/ScriptableObjects/MapConfig.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Railroad/MapConfig")]
+public class MapConfig : ScriptableObject
+{
+	public List<string> StationNames;
+}
