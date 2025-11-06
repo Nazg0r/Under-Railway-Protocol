0987f40fc2ef181d4e2ebc419f6629573571332a
Nazarii <gorchinshiy83@gmail.com>
2025-11-06 16:22:31 +0200

    create Map class

---
+++ Assets/Scripts/MapGeneration/Map.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+public class Map : MapBase<Station, PathMap>
+{
+	public Station InitialStation { get; set; }
+
+	public Map(Station initialStation)
+	{
+		InitialStation = initialStation;
+	}
+
+	public List<Station> GetStations() =>
+		GetNodes(InitialStation);
+
+	public List<PathMap> GetPathMaps() =>
+		GetConnections(InitialStation);
+
+	public Station GetStationByName(string name) =>
+		GetNodeByName(InitialStation, name);
+}
