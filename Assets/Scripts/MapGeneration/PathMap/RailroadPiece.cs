b57a93d844c574f20ea5693bb3488aec3498d6a0
Nazarii <gorchinshiy83@gmail.com>
2025-11-09 00:54:27 +0200

    extend class RailroadPiece

--- Assets/Scripts/MapGeneration/PathMap/RailroadPiece.cs
+++ Assets/Scripts/MapGeneration/PathMap/RailroadPiece.cs
@@ -2,6 +2,17 @@
 
 public class RailroadPiece
 {
-	public string Type { get; set; }
-	public Vector3 Position { get; set; }
+	public DirectionType Type { get;}
+	public Vector2 StartPoint { get; set; }
+	public Vector2 EndPoint { get; set; }
+	public Quaternion Rotation { get;}
+
+	public RailroadPiece(DirectionType type, Vector2 startPoint, Vector2 endPoint)
+	{
+		Type = type;
+		StartPoint = startPoint;
+		EndPoint = endPoint;
+		Rotation = Quaternion.identity;
+		Rotation.eulerAngles.Set(0f, (float)type, 0f);
+	}
 }
