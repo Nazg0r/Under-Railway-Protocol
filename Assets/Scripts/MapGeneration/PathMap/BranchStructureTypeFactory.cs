55ae643d01570406a936b0cb261127bdb8c211d0
Nazarii <gorchinshiy83@gmail.com>
2025-11-09 00:53:49 +0200

    create factory for BranchStructureType instances

---
+++ Assets/Scripts/MapGeneration/PathMap/BranchStructureTypeFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BranchStructureTypeFactory
+{
+	private readonly BranchType[] _branchTypes = Resources.LoadAll<BranchType>("Branches/Types/");
+
+	public BranchStructureType Create(string name)
+	{
+		BranchType type = _branchTypes.FirstOrDefault(t => t.Name == name);
+
+		if (type == null) 
+			throw new NullReferenceException($"Branch type with name \"{name}\" does not exist");
+
+		List<RailroadPiece> directions = type.Directions
+			.Select(direction => 
+				new RailroadPiece(
+					direction.Type,
+					direction.StartPoint,
+					direction.EndPoint))
+			.ToList();
+
+		return new BranchStructureType(type.Name, directions);
+	}
+}
