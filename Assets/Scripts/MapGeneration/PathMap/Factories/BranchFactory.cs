0b403bbd23f5ca20503a301aad1653e1779ee2e6
Nazarii <gorchinshiy83@gmail.com>
2025-11-11 21:31:29 +0200

    reorganize factories code

---
+++ Assets/Scripts/MapGeneration/PathMap/Factories/BranchFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class BranchFactory
+{
+	private readonly BranchStructureTypeFactory _branchTypeFactory = new();
+
+	public List<Branch> Create(PathMapConfig config, BranchStructures structures, string pathMapName)
+	{
+		List<Branch> branches = new(config.BranchNum);
+
+		for (var i = 0; i < config.BranchNum; i++)
+		{
+			string name = $"{pathMapName}-Branch{i}";
+			var structureName = structures
+				.Bindings
+				.FirstOrDefault(b => b.BranchIndex == i)
+				.StructureName;
+			var type = _branchTypeFactory.Create(structureName);
+			branches.Add(new Branch(type, name));
+		}
+
+		return branches;
+	}
+}
