6dcb57312b009001b835d1d2628e45c472f65806
Nazarii <gorchinshiy83@gmail.com>
2025-11-11 21:29:55 +0200

    add BasePoint property to Branch class

--- Assets/Scripts/MapGeneration/PathMap/Branch.cs
+++ Assets/Scripts/MapGeneration/PathMap/Branch.cs
@@ -1,10 +1,23 @@
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class Branch : IMapNode<Railroad>
 {
+	private Vector2 _basePoint = Vector2.zero;
 	public string Name { get; }
 	public BranchStructureType Type { get; }
 	public List<Railroad> Roads { get; } = new();
+	public Vector2 BasePoint
+	{
+		get => _basePoint;
+		set
+		{
+			if (_basePoint == Vector2.zero) _basePoint = value;
+			else if (_basePoint != value)
+				throw new ArgumentException($"Base points are not equal. {_basePoint} vs {value}");
+		}
+	}
 
 	List<Railroad> IMapNode<Railroad>.Connections => Roads;
 
