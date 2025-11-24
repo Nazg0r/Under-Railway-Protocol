5ad3eb3ffce6c8026988e68c7d6ce9b9d340addc
Nazarii <gorchinshiy83@gmail.com>
2025-11-24 10:54:32 +0200

    put MapMonitorPreparationManager in GamePreparationManager

---
+++ Assets/Scripts/Managers/Core/Preparation/GamePreparationManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GamePreparationManager : MonoBehaviour
+{
+	[SerializeField] MapGenerationManager _mapGenerationManager;
+	[SerializeField] MapMonitorPreparationManager _mapMonitorPreparationManager;
+	private GameManager _gameManager;
+
+	private void Awake()
+	{
+		ObjectResolver.Register(this);
+		_gameManager = ObjectResolver.Resolve<GameManager>();
+
+		_gameManager.OnGameStateChanged += PrepareGame;
+	}
+
+	private void PrepareGame(GameState state)
+	{
+		if (state == GameState.Preparation)
+		{
+			_mapGenerationManager.Initialize();
+			_mapMonitorPreparationManager.Initialize();
+		}
+	}
+}
