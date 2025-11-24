1a1b1e144a8569df2e2bdecb73b9b74201228569
Nazarii <gorchinshiy83@gmail.com>
2025-11-24 10:49:22 +0200

    implement MapMonitorInputController

---
+++ Assets/Scripts/Controllers/MapMonitor/MapMonitorInputController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MapMonitorInputController : MonoBehaviour
+{
+	private GameInputs _inputs;
+
+	private MapMonitorEventBus _eventBus;
+
+	private void Awake()
+	{
+		_inputs = new GameInputs();
+	}
+
+	public void Initialize()
+	{
+		ObjectResolver.Register(this);
+		_eventBus = ObjectResolver.Resolve<MapMonitorEventBus>();
+		gameObject.SetActive(false);
+	}
+
+	private void OnEnable()
+	{
+		_inputs.Enable();
+		_inputs.MapMonitor.BackToMenu.performed += OnBackToMenuPressed;
+		_inputs.MapMonitor.BuildPath.performed += OnBuildPathPressed;
+	}
+
+	private void OnDisable()
+	{
+		_inputs.MapMonitor.BackToMenu.performed -= OnBackToMenuPressed;
+		_inputs.MapMonitor.BuildPath.performed -= OnBuildPathPressed;
+		_inputs.Disable();
+	}
+
+	private void OnBackToMenuPressed(InputAction.CallbackContext ctx) =>
+		_eventBus.onBackPressed.Invoke();
+
+	private void OnBuildPathPressed(InputAction.CallbackContext ctx) =>
+		_eventBus.onBuildPathPressed.Invoke();
+}
